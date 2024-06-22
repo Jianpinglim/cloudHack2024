f3b0b2c45ed72510331d9404f897fc2120e2c206
Jianpinglim <jianpinglim62@gmail.com>
2024-06-22 11:12:01 +0800

    started on hand tracking

--- main.py
+++ main.py
@@ -6,20 +6,29 @@ import os
 
 # settng up hand model and stuff to draw the joints
 mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hand #hand model
+mp_hands = mp.solutions.hands #hand model
 
 # init the webcam
 
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
+#set confidence level stuff loop
+with mp.hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: #detect hand at 80% and tracking at 50%
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    cv2.imshow('hand tracking', frame)
+        #detecttions
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    #quit using q btn
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        cv2.imshow('hand tracking', frame)
+
+        #quit using q btn
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
