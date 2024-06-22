db497bf0b13db7d941967f24f37c8b688df3ad76
Jianpinglim <jianpinglim62@gmail.com>
2024-06-22 10:56:16 +0800

    initialised cv2 and logic to open it and close

--- main.py
+++ main.py
@@ -3,3 +3,23 @@ import mediapipe as mp
 import cv2
 import numpy as np
 import os
+
+# settng up hand model and stuff to draw the joints
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hand #hand model
+
+# init the webcam
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    cv2.imshow('hand tracking', frame)
+
+    #quit using q btn
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
