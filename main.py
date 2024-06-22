81c9ee9ebe2c7c0dbbb0aad6c57d2237342540aa
Jianpinglim <jianpinglim62@gmail.com>
2024-06-22 15:25:50 +0800

    added flappybird

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands #hand model
 
 #create a touch threshold for how far the finger can be considered as touching
-touchThreshold = 0.05 #change to suit later
+touchThreshold = 0.03 #change to suit later
 #we create a function for calculating the distance
 
 def calDist(p1, p2):
@@ -20,8 +20,8 @@ def calDist(p1, p2):
 #play the sound in a diff function, thread
 def playSound(file):
     ps(file)
-# init the webcam
 
+# init the webcam
 cap = cv2.VideoCapture(0)
 #set confidence level stuff loop
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: #detect hand at 80% and tracking at 50%
