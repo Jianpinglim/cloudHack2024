0b78ccfd3d34f4bbcf000c7aaa3894c45678558d
Jianpinglim <jianpinglim62@gmail.com>
2024-06-22 15:59:17 +0800

    this part the obstacles are drawn just too fast

--- flappyBird.py
+++ flappyBird.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 SCREEN = pygame.display.set_mode((500, 750))
@@ -15,15 +16,42 @@ running = True
 def displayPlayer(x, y):
     SCREEN.blit(playerImg, (x, y))
 
+# Pipes stuff
+obs_width = 70
+obs_ht = random.randint(150, 450)
+obs_clr = (211, 253, 117)
+obs_x_change = -6
+obs_x = 500
+
+def display_obstacle(height):
+    top_obs_height = height
+    bottom_obs_height = 635 - height - 150  # Calculate the bottom part height based on top height
+
+    pygame.draw.rect(SCREEN, obs_clr, (obs_x, 0, obs_width, top_obs_height))
+    pygame.draw.rect(SCREEN, obs_clr, (obs_x, 635 - bottom_obs_height, obs_width, bottom_obs_height))
+
 while running:
     SCREEN.fill((0, 0, 0))
     SCREEN.blit(bgImg, (0, 0))
 
     for event in pygame.event.get():
-        if event.type == pygame.quit:
+        if event.type == pygame.QUIT:
             running = False
 
     playerY += playerYChange
+
+    if playerY <= 0:
+        playerY = 0
+    if playerY >= 571:
+        playerY = 571
+
+    obs_x += obs_x_change
+    if obs_x <= -obs_width:  # Check if obstacle has moved off the screen
+        obs_x = 500
+        obs_ht = random.randint(200, 400)  # Generate new random height for next obstacle
+
+    display_obstacle(obs_ht)
+
     displayPlayer(playerX, playerY)
     pygame.display.update()
 
